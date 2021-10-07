074f933a0533c7ae9529035d2794196d6bc25a14
k.plata <k.plata@metrological.com>
2021-10-07 11:16:24 +0000

    Skip past the plaintext header in server response

--- src/ResponseCallback.cpp
+++ src/ResponseCallback.cpp
@@ -31,23 +31,26 @@ namespace CENCDecryptor {
         }
 
         void ResponseCallback::Response(Core::ProxyType<Web::Request> req,
-            Core::ProxyType<Web::Response> res)
-        {
-            ASSERT(_session != nullptr);
-            
+                    Core::ProxyType<Web::Response> res)
+        {          
             if (req.IsValid() && res->HasBody()) {
                 Core::ProxyType<Web::TextBody> body = res->Body<Web::TextBody>();
-                auto keyResponse = reinterpret_cast<const uint8_t*>(body->c_str());
+
+                std::string drmHeadAnchor = "\r\n\r\n";
+                size_t wvDrmHeadPos = body->find(drmHeadAnchor);
+
+                // Some keysystems (usually WV) add additional information about the keyId's
+                // in the beggining of the body. Let's skip past that bit if it's detected:
+                auto newIndex = (wvDrmHeadPos != std::string::npos) ? (wvDrmHeadPos + drmHeadAnchor.length()) : 0;
+                auto keyResponse = reinterpret_cast<const uint8_t*>(body->substr(newIndex).c_str());
 
                 _sessionLock.Lock();
-
-                opencdm_session_update(_session, keyResponse, body->length());
-
+                OpenCDMError result = opencdm_session_update(_session, keyResponse, body->length() - newIndex);
                 _sessionLock.Unlock();
-            } else {
-                TRACE_L1("Challenge response without a body");
+                } else {
+                    fprintf(stderr, "Challenge response without a body");
+                }
             }
         }
-    }
 }
 }
