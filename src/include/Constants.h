d83b2faa28428a373d2b555f61353790aaba30c0
k.plata <k.plata@metrological.com>
2021-10-18 10:56:22 +0000

    Add seperate file for constants

---
+++ src/include/Constants.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright RDK Management
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation, version 2
+ * of the license.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+*/
+
+#pragma once
+
+namespace CENCDecryptor {
+namespace Constants {
+    constexpr static auto LogPrefix = "[GstCencDecrypt]:[Log]: ";
+    constexpr static auto ErrorPrefix = "[GstCencDecrypt]:[Error]: ";
+    constexpr static auto WaitForKeyTimeInMs = 5000;
+}
+}
